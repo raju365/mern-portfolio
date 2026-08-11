6dee8437b4f869c805a087d487c9ff8a03f02f7f
raju barman <rajubarman7665@gmail.com>
2026-08-11 18:27:53 +0530

    update new project

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,7 +1,24 @@
-import recipeApp from "../images/recipeApp.png";
+import orivai from "../images/OrivAi.png";
 import shopx from "../images/shopx.png";
-import photoTune from "../images/PhotoTune_site.png"
+import photoTune from "../images/PhotoTune_site.png";
 export const projects = [
+  {
+    title: "Oriv AI - Full Stack AI Voice Assistant",
+    desc: "Developed a real-time AI voice assistant with chat history, authentication, and natural speech interaction using the MERN stack and Socket.IO.",
+    tech: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Socket.IO",
+      "Gemini API",
+      "Tailwind CSS",
+      "React Three Fiber",
+    ],
+    live: "https://oriv-ai.vercel.app/",
+    github: "https://github.com/raju365/Oriv-AI",
+    image: orivai,
+  },
   {
     title: "ShopX E-Commerce",
     desc: "React + Redux based e-commerce store with authentication, admin panel, infinite scroll, and full cart functionality.",
@@ -17,22 +34,12 @@ export const projects = [
     github: "https://github.com/raju365/ReactPractices/tree/main/project2",
     image: shopx,
   },
-
   {
-    title: "Recipe App",
-    desc: "Modern recipe app with search & category filters",
-    tech: ["React", "API"],
-    live: "https://recipeapp-raju365.vercel.app/",
-    github: "https://github.com/raju365/ReactPractices/tree/main/project1",
-    image: recipeApp,
+    title: "PhotoTune – Image Editing App",
+    desc: "Web-based image editor with real-time filters, tools, and 4K export support",
+    tech: ["HTML5 Canvas", "CSS3", "JavaScript"],
+    live: "https://phototune-raju365.vercel.app/",
+    github: "https://github.com/raju365/photoTune-imageEditor",
+    image: photoTune,
   },
-  {
-  title: "PhotoTune – Image Editing App",
-  desc: "Web-based image editor with real-time filters, tools, and 4K export support",
-  tech: ["HTML5 Canvas", "CSS3", "JavaScript"],
-  live: "https://phototune-raju365.vercel.app/",
-  github: "https://github.com/raju365/photoTune-imageEditor",
-  image: photoTune,
-}
-
 ];
